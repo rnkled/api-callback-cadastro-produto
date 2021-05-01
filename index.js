521cd3bf7792f046a15c4006c0692ff50ec9f0ae
Kener Wendel <kenerwendel@gmail.com>
2021-05-01 10:17:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 8888;
+
+app.use(cors());
+
+app.use(express.json());
+
+var dados = []
+var quantidade = 0
+var date = new Date(Date.now())
+
+app.get('/', (req, res) => {
+    res.send({monitoramento: date.toLocaleString('pt-BR'), quantidade: quantidade, dados: dados});
+    // dados = []
+    // quantidade = 0
+});
+app.post('/', async (req, res) => {
+    try {    
+        dados.push(req.body)
+        quantidade += 1
+        res.send({status:"recebido"})   
+    } catch (error) {
+        res.status(400).send({error: error})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`API listening at http://localhost:${port}`);
+});
